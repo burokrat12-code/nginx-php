b008223b36b34595ca6755e319cfe28bc726811c
burokrat12-code <burokrat12@gmail.com>
2026-04-23 14:38:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-envsubst '${MAIN_DOMAIN} ${PUSH_DOMAIN} ${MAIL_DOMAIN} ${WEB_ROOT} ${NTFY_BACKEND} ${NTFY_UserAgent} ${NTFY_SECRET_PATH} ${MAIL_BACKEND} ${MAIL_SECRET_PATH} ${VLESS_DOMAIN} ${VLESS_BACKEND}' \
+# Экранируем точки в IP для использования в регулярных выражениях
+# Используем одинарный бэкслеш для правильного экранирования
+export IP_SERV_ESCAPED=$(echo "${IP_SERV}" | sed 's/\./\\./g')
+
+envsubst '${MAIN_DOMAIN} ${PUSH_DOMAIN} ${MAIL_DOMAIN} ${WEB_ROOT} ${NTFY_BACKEND} ${NTFY_UserAgent} ${NTFY_SECRET_PATH} ${MAIL_BACKEND} ${MAIL_SECRET_PATH} ${HAPROXY_IP} ${IP_SERV_ESCAPED} ${VLESS_DOMAIN} ${VLESS_BACKEND} ${VLESS_XHTTP_DOMAIN}' \
     < /etc/nginx/nginx.conf.template \
     > /etc/nginx/nginx.conf
 
